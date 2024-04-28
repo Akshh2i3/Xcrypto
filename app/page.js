fcc85cd842fa7c86f913c49a256428f589a75af0
Akshh2i3 <akshaysethiya23@gmail.com>
2024-04-28 13:27:41 +0530

    improved home page

--- app/page.js
+++ app/page.js
@@ -3,12 +3,11 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='h-[84vh] bg-slate-800 '>
+    <div className='h-fit pb-[2vw] bg-slate-800 flex flex-col justify-evenly'>
       <div className='w-1/3 mx-auto'>
         <img src='../img/btc.png' alt='homePageImg' />
       </div>
       <p className='text-white mix-blend-difference text-center text-7xl'>Xcrypto</p>
-
     </div>
   )
 }
